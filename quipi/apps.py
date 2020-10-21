457077d06690c8264eba7e9ed03659b7bf2ac87c
Jerrica Bobadilla <jerrica@Jerricas-iMac-5.home>
2020-10-21 16:57:39 -0700

    initial file set up

---
+++ quipi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuipiConfig(AppConfig):
+    name = 'quipi'
---
+++ settings.sample.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE dbname;
+CREATE USER username WITH PASSWORD 'password';
+GRANT ALL PRIVILEGES ON DATABASE dbname TO username;
