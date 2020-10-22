73b7e1df911621c98a87900bd9a1de91a2af901e
Jerrica Bobadilla <jerrica@Jerricas-iMac-5.home>
2020-10-22 11:07:13 -0700

    show prompt text on quip

--- quipi/serializers.py
+++ quipi/serializers.py
@@ -3,9 +3,11 @@ from .models import Prompt
 from .models import Quip
 
 class QuipSerializer(serializers.ModelSerializer):
+    prompt = serializers.StringRelatedField(many=False)
+
     class Meta:
         model = Quip
-        fields = ('__all__')
+        fields = ('prompt', 'prompt_id', 'text', 'times_played', 'times_chosen', 'creator_name', 'accepted')
 
 class PromptSerializer(serializers.ModelSerializer):
     quips = QuipSerializer(many=True, read_only=True)
